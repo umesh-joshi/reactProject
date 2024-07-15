38bfdca1decfbd4c2701bbe7c8663598c11a704f
I517477 <u.joshi@sap.com>
2024-07-15 19:13:28 +0530

    fixed tailwind issue

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl bg-green-500 p-3 rounded-md'>Vite with Tailwind</h1>
+
+      <Card username='Umesh' role='Manager'/>
+      <Card />
+      <Card />
+    </>
+  )
+}
+
+export default App
