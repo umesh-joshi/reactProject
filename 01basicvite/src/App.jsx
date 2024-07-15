1127c16693a9f6b13dd1d38da6be5290d4ac9366
I517477 <u.joshi@sap.com>
2024-07-15 18:31:50 +0530

    CustomReact designed

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <div>
-      <h1>Vite react app</h1>
+      <h1>Vite react app {2+2}</h1>
       <h1>Vite react app</h1>
       <Youtube />
     </div>
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,8 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const AnotherElement = (
+    <a href='https://www.google.com' target='_blank'>Click me to visit google</a>
+)
+
+const AreactElement = React.createElement(
+    'a', 
+    {href: 'https://www.google.com', target: '_blank'},
+    'Click me to visit google'
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    <App   />
 
 )
